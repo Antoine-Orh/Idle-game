521b9dcd851b08e1a6a803844e30e3f3ee040061
Antoine <antoine@orhant.net>
2021-05-18 09:11:20 +0200

    +tests d'assets

--- src/game.js
+++ src/game.js
@@ -1,30 +1,13 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(1875, 970, Phaser.AUTO, '');
+
 
 game.state.add('play', {
     preload: function() {
-        this.game.load.image('forest-back', 'assets/parallax_forest_pack/layers/parallax-forest-back-trees.png');
-        this.game.load.image('forest-lights', 'assets/parallax_forest_pack/layers/parallax-forest-lights.png');
-        this.game.load.image('forest-middle', 'assets/parallax_forest_pack/layers/parallax-forest-middle-trees.png');
-        this.game.load.image('forest-front', 'assets/parallax_forest_pack/layers/parallax-forest-front-trees.png');
+        this.game.load.image('mine', 'assets/background/mine.jpg');
 
-        this.game.load.image('aerocephal', 'assets/allacrost_enemy_sprites/aerocephal.png');
-        this.game.load.image('arcana_drake', 'assets/allacrost_enemy_sprites/arcana_drake.png');
-        this.game.load.image('aurum-drakueli', 'assets/allacrost_enemy_sprites/aurum-drakueli.png');
-        this.game.load.image('bat', 'assets/allacrost_enemy_sprites/bat.png');
-        this.game.load.image('daemarbora', 'assets/allacrost_enemy_sprites/daemarbora.png');
-        this.game.load.image('deceleon', 'assets/allacrost_enemy_sprites/deceleon.png');
-        this.game.load.image('demonic_essence', 'assets/allacrost_enemy_sprites/demonic_essence.png');
-        this.game.load.image('dune_crawler', 'assets/allacrost_enemy_sprites/dune_crawler.png');
-        this.game.load.image('green_slime', 'assets/allacrost_enemy_sprites/green_slime.png');
-        this.game.load.image('nagaruda', 'assets/allacrost_enemy_sprites/nagaruda.png');
-        this.game.load.image('rat', 'assets/allacrost_enemy_sprites/rat.png');
-        this.game.load.image('scorpion', 'assets/allacrost_enemy_sprites/scorpion.png');
-        this.game.load.image('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png');
-        this.game.load.image('snake', 'assets/allacrost_enemy_sprites/snake.png');
-        this.game.load.image('spider', 'assets/allacrost_enemy_sprites/spider.png');
-        this.game.load.image('stygian_lizard', 'assets/allacrost_enemy_sprites/stygian_lizard.png');
 
-        this.game.load.image('gold_coin', 'assets/496_RPG_icons/I_GoldCoin.png');
+        this.game.load.image('stone', 'assets/stone/stone.jpg');
+
 
         this.game.load.image('dagger', 'assets/496_RPG_icons/W_Dagger002.png');
         this.game.load.image('swordIcon1', 'assets/496_RPG_icons/S_Sword15.png');
@@ -65,7 +48,7 @@ game.state.add('play', {
 
         this.background = this.game.add.group();
         // setup each of our background layers to take the full screen
-        ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+        ['mine', 'mine', 'mine', 'mine']
             .forEach(function(image) {
                 var bg = state.game.add.tileSprite(0, 0, state.game.world.width,
                     state.game.world.height, image, '', state.background);
@@ -98,22 +81,8 @@ game.state.add('play', {
         });
 
         var monsterData = [
-            {name: 'Aerocephal',        image: 'aerocephal',        maxHealth: 10},
-            {name: 'Arcana Drake',      image: 'arcana_drake',      maxHealth: 20},
-            {name: 'Aurum Drakueli',    image: 'aurum-drakueli',    maxHealth: 30},
-            {name: 'Bat',               image: 'bat',               maxHealth: 5},
-            {name: 'Daemarbora',        image: 'daemarbora',        maxHealth: 10},
-            {name: 'Deceleon',          image: 'deceleon',          maxHealth: 10},
-            {name: 'Demonic Essence',   image: 'demonic_essence',   maxHealth: 15},
-            {name: 'Dune Crawler',      image: 'dune_crawler',      maxHealth: 8},
-            {name: 'Green Slime',       image: 'green_slime',       maxHealth: 3},
-            {name: 'Nagaruda',          image: 'nagaruda',          maxHealth: 13},
-            {name: 'Rat',               image: 'rat',               maxHealth: 2},
-            {name: 'Scorpion',          image: 'scorpion',          maxHealth: 2},
-            {name: 'Skeleton',          image: 'skeleton',          maxHealth: 6},
-            {name: 'Snake',             image: 'snake',             maxHealth: 4},
-            {name: 'Spider',            image: 'spider',            maxHealth: 4},
-            {name: 'Stygian Lizard',    image: 'stygian_lizard',    maxHealth: 20}
+            {name: 'Stone',        image: 'stone',        maxHealth: 10},
+
         ];
         this.monsters = this.game.add.group();
 
@@ -177,31 +146,18 @@ game.state.add('play', {
             this.dmgTextPool.add(dmgText);
         }
 
-        // create a pool of gold coins
-        this.coins = this.add.group();
-        this.coins.createMultiple(50, 'gold_coin', '', false);
-        this.coins.setAll('inputEnabled', true);
-        this.coins.setAll('goldValue', 1);
-        this.coins.callAll('events.onInputDown.add', 'events.onInputDown', this.onClickCoin, this);
-
-        this.playerGoldText = this.add.text(30, 30, 'Gold: ' + this.player.gold, {
-            font: '24px Arial Black',
-            fill: '#fff',
-            strokeThickness: 4
-        });
-
         // 100ms 10x a second
         this.dpsTimer = this.game.time.events.loop(100, this.onDPS, this);
 
         // setup the world progression display
         this.levelUI = this.game.add.group();
         this.levelUI.position.setTo(this.game.world.centerX, 30);
-        this.levelText = this.levelUI.addChild(this.game.add.text(0, 0, 'Level: ' + this.level, {
+        this.levelText = this.levelUI.addChild(this.game.add.text(0, 0, 'Mine: ' + this.level, {
             font: '24px Arial Black',
             fill: '#fff',
             strokeThickness: 4
         }));
-        this.levelKillsText = this.levelUI.addChild(this.game.add.text(0, 30, 'Kills: ' + this.levelKills + '/' + this.levelKillsRequired, {
+        this.levelKillsText = this.levelUI.addChild(this.game.add.text(0, 30, 'blocs minés: ' + this.levelKills + '/' + this.levelKillsRequired, {
             font: '24px Arial Black',
             fill: '#fff',
             strokeThickness: 4
@@ -232,37 +188,9 @@ game.state.add('play', {
             button.details.purchaseHandler.call(this, button, this.player);
         }
     },
-    onClickCoin: function(coin) {
-        if (!coin.alive) {
-            return;
-        }
-        // give the player gold
-        this.player.gold += coin.goldValue;
-        // update UI
-        this.playerGoldText.text = 'Gold: ' + this.player.gold;
-        // remove the coin
-        coin.kill();
-    },
     onKilledMonster: function(monster) {
         // move the monster off screen again
         monster.position.set(1000, this.game.world.centerY);
-
-        var coin;
-        // spawn a coin on the ground
-        coin = this.coins.getFirstExists(false);
-        coin.reset(this.game.world.centerX + this.game.rnd.integerInRange(-100, 100), this.game.world.centerY);
-        coin.goldValue = Math.round(this.level * 1.33);
-        this.game.time.events.add(Phaser.Timer.SECOND * 3, this.onClickCoin, this, coin);
-
-        this.levelKills++;
-
-        if (this.levelKills >= this.levelKillsRequired) {
-            this.level++;
-            this.levelKills = 0;
-        }
-
-        this.levelText.text = 'Level: ' + this.level;
-        this.levelKillsText.text = 'Kills: ' + this.levelKills + '/' + this.levelKillsRequired;
 
         // pick a new monster
         this.currentMonster = this.monsters.getRandom();
